31bf3fb7d4a86be5aec3052daed0168264fb4e6c
minjaeli <minjaelee@comcom.ai>
2025-11-04 08:13:59 +0900

    set: class type

--- main.py
+++ main.py
@@ -1,62 +1,74 @@
-import dotenv
+from dotenv import load_dotenv
 
-dotenv.load_dotenv()
+load_dotenv()
 
-from crewai import Crew, Agent, Task
-from crewai.project import CrewBase, task, agent, crew
+from crewai import Agent, Crew, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.knowledge.source.text_file_knowledge_source import TextFileKnowledgeSource
 from models import JobList, RankedJobList, ChosenJob
 from tools import web_search_tool
 
-
 @CrewBase
 class JobHunterCrew:
-
     @agent
-    def job_search_agent(self):
+    def job_search_agent(self) -> Agent:
         return Agent(
             config=self.agents_config["job_search_agent"],
             tools=[web_search_tool],
         )
 
     @agent
-    def job_matching_agent(self):
-        return Agent(config=self.agents_config["job_matching_agent"])
+    def job_matching_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["job_matching_agent"],
+            knowledge_sources=[TextFileKnowledgeSource(file_paths=["resume.txt"])],
+        )
 
     @agent
-    def resume_optimization_agent(self):
-        return Agent(config=self.agents_config["resume_optimization_agent"])
+    def resume_optimization_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["resume_optimization_agent"],
+            knowledge_sources=[TextFileKnowledgeSource(file_paths=["resume.txt"])],
+        )
 
     @agent
-    def company_research_agent(self):
-        return Agent(config=self.agents_config["company_research_agent"])
+    def company_research_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["company_research_agent"],
+            knowledge_sources=[TextFileKnowledgeSource(file_paths=["resume.txt"])],
+            tools=[web_search_tool],
+        )
 
     @agent
-    def interview_prep_agent(self):
-        return Agent(config=self.agents_config["interview_prep_agent"])
+    def interview_prep_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["interview_prep_agent"],
+            knowledge_sources=[TextFileKnowledgeSource(file_paths=["resume.txt"])],
+        )
 
     @task
-    def job_extraction_task(self):
+    def job_extraction_task(self) -> Task:
         return Task(
             config=self.tasks_config["job_extraction_task"],
             output_pydantic=JobList,
         )
 
     @task
-    def job_matching_task(self):
+    def job_matching_task(self) -> Task:
         return Task(
             config=self.tasks_config["job_matching_task"],
             output_pydantic=RankedJobList,
         )
 
     @task
-    def job_selection_task(self):
+    def job_selection_task(self) -> Task:
         return Task(
             config=self.tasks_config["job_selection_task"],
             output_pydantic=ChosenJob,
         )
 
     @task
-    def resume_rewriting_task(self):
+    def resume_rewriting_task(self) -> Task:
         return Task(
             config=self.tasks_config["resume_rewriting_task"],
             context=[
@@ -65,7 +77,7 @@ class JobHunterCrew:
         )
 
     @task
-    def company_research_task(self):
+    def company_research_task(self) -> Task:
         return Task(
             config=self.tasks_config["company_research_task"],
             context=[
@@ -74,7 +86,7 @@ class JobHunterCrew:
         )
 
     @task
-    def interview_prep_task(self):
+    def interview_prep_task(self) -> Task:
         return Task(
             config=self.tasks_config["interview_prep_task"],
             context=[
@@ -85,7 +97,7 @@ class JobHunterCrew:
         )
 
     @crew
-    def crew(self):
+    def crew(self) -> Crew:
         return Crew(
             agents=self.agents,
             tasks=self.tasks,
@@ -93,4 +105,17 @@ class JobHunterCrew:
         )
 
 
-JobHunterCrew().crew().kickoff()
+result = (
+    JobHunterCrew()
+    .crew()
+    .kickoff(
+        inputs={
+            "level": "Senior",
+            "position": "Web3 Developer",
+            "location": "Germany",
+        }
+    )
+)
+
+for task_output in result.tasks_output:
+    print(task_output.pydantic)
